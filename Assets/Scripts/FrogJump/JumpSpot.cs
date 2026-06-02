f5381831071aeb010bfbf0174576babb45b7c9ac
LrkRtr <karl.reiter16@gmail.com>
2026-06-02 14:38:24 -0400

    JumpSpot Size Fix

--- Assets/Scripts/FrogJump/JumpSpot.cs
+++ Assets/Scripts/FrogJump/JumpSpot.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 public class JumpSpot : MonoBehaviour
 {
     public bool isReserved = false; //Has a frog on it or jumping to it
-    public bool sizeScaled = true; //Use the scale of the object as the size?
-    public float size = 1f; //What size of frog can be on the pad
 
     public bool isTilted = false; //Is a pad on a wall, needs to jump to "nextPad"
     [SerializeField] private GameObject nextPad; //The next pad to jump to
@@ -14,12 +12,6 @@ public class JumpSpot : MonoBehaviour
     public GameObject getNextPad()
     {
         return nextPad;
-    }
-    public float getSize()
-    {
-        if (sizeScaled)
-            return transform.localScale.x;
-        return size;
     }
     public void Reserve()
     {
