548d325f77320f56647414718b561b54d7fc5541
annas2727 <73410623+annas2727@users.noreply.github.com>
2026-06-02 00:51:36 -0400

    colotrs

---
+++ Assets/Scripts/FrogProperties.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class FrogProperties : MonoBehaviour
+{
+    public enum bodyColorName { red, orange, yellow, green, blue, purple }
+    public enum patternColorName { red, orange, yellow, green, blue, purple }
+    public enum pattern { spots, stripes, none }
+
+    [SerializeField] bodyColorName currentBodyColorName;
+    [SerializeField] patternColorName currentPatternColorName;
+    [SerializeField] pattern currentPatternType;
+
+    private string bodyColor; 
+    private string patternColor;
+    private string patternType;
+
+    public Material frogMaterial;
+
+    private Renderer frogRenderer;
+    GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+
+        bodyColor = currentBodyColorName.ToString();
+        patternColor = currentPatternColorName.ToString();
+        patternType = currentPatternType.ToString();
+
+        bodyColor = gameManager.colors.ContainsKey(bodyColor) ? gameManager.colors[bodyColor] : "#ffffff";
+        patternColor = gameManager.colors.ContainsKey(patternColor) ? gameManager.colors[patternColor] : "#ffffff";
+        patternType = gameManager.patternTypes.Contains(patternType) ? patternType : gameManager.patternTypes[0];
+
+        frogRenderer = GetComponent<Renderer>();
+
+        if (frogMaterial != null)
+            frogRenderer.material = frogMaterial;
+        else
+            frogMaterial = frogRenderer.material;
+            
+        ApplyProperties();
+    }
+
+    public void ApplyProperties()
+    {
+        if (frogMaterial == null) return;
+
+        SetColor("_Body", bodyColor);
+        SetColor("_Spots", patternColor);
+        SetColor("_Stripes", patternColor); 
+        //SetPatternType(patternType);
+    }
+
+    private void SetColor(string propertyName, string hex)
+    {
+        if (ColorUtility.TryParseHtmlString(hex, out Color color))
+            frogMaterial.SetColor(propertyName, color);
+        else
+            Debug.LogWarning($"Invalid hex color: {hex}");
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public Dictionary<string, string> colors = new Dictionary<string, string>
+    {
+        { "red", "#ff0000" },
+        { "orange", "#ff9900" },
+        { "yellow", "#ffd500" },
+        { "green", "#00ff00" },
+        { "blue", "#0000ff" },
+        { "purple", "#6f00ff" }
+    };
+
+    public List<string> patternTypes = new List<string>
+    {
+        "spots",
+        "stripes",
+        "none"
+    };
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
