0fe765dd24cce8aeb48fb5e5d3171b94d8b41204
annas2727 <73410623+annas2727@users.noreply.github.com>
2026-06-03 01:05:44 -0400

    frogs

--- Assets/FrogGame/Scripts/FrogJump/AnimateFrog.cs
+++ Assets/FrogGame/Scripts/FrogJump/AnimateFrog.cs
@@ -9,21 +9,10 @@ public class AnimateFrog : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        animator.SetBool("Jump", false);
-        animator.SetBool("Idle", true);
     }
 
     public void SetAnimationState(string state)
     {
-        animator.SetBool("Idle", state == "Idle");
-        animator.SetBool("J_Takeoff", state == "J_Takeoff");
-        animator.SetBool("J_Mid", state == "J_Mid");
-        animator.SetBool("J_Land", state == "J_Land");
-        animator.SetBool("P_Takeoff", state == "P_Takeoff");
-        animator.SetBool("P_Mid", state == "P_Mid");
-        animator.SetBool("P_Land", state == "P_Land");
-        animator.SetBool("Swim", state == "Swim");
-        animator.SetBool("Croak", state == "Croak");
-
+        animator.Play(state);
     }
 }
