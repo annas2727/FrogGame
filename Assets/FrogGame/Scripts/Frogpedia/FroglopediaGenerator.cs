ed9a36d5153cbf7a2fb0efd1b3f8a583720ada0c
annas2727 <73410623+annas2727@users.noreply.github.com>
2026-07-27 01:52:42 -0400

    Added text

--- Assets/FrogGame/Scripts/Frogpedia/FroglopediaGenerator.cs
+++ Assets/FrogGame/Scripts/Frogpedia/FroglopediaGenerator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 
 public class FroglopediaGenerator : MonoBehaviour
 {
@@ -11,36 +12,45 @@ public class FroglopediaGenerator : MonoBehaviour
         public Texture2D texture;
     }
 
+    [Header("Book")]
     public Book book;
+
+    [Header("Cameras")]
     public Camera pageCamera;
-    public Renderer[] frogRenderers; // drag 9 quads here
+    public Camera tocCamera;
+    public TMP_Text tocText;
+
+    [Header("Frog Rendering")]
+    public Renderer[] frogRenderers;
     public FrogPattern[] patterns;
+
+    [Header("Book Pages")]
     public Sprite front;
-    public Sprite TOC; 
     public Sprite back;
-    
+
+    [Header("Background")]
+    public Renderer backgroundRenderer;
+    public Texture2D[] templateBackgrounds;
+
     private List<Color> bodyColorList = new List<Color>();
     private List<Color> patternColorList = new List<Color>();
     private List<Sprite> generatedPages = new List<Sprite>();
-    public Renderer backgroundRenderer; // the plane behind the quads
-    public Texture2D[] templateBackgrounds; // your 4 template textures
-    
-    GameManager gm; 
+
+    GameManager gm;
+
     void Awake()
     {
         gm = FindObjectOfType<GameManager>();
 
         foreach (var hex in gm.bodyColors.Values)
         {
-            Color col;
-            if (ColorUtility.TryParseHtmlString(hex, out col))
+            if (ColorUtility.TryParseHtmlString(hex, out Color col))
                 bodyColorList.Add(col);
         }
 
         foreach (var hex in gm.patternColors.Values)
         {
-            Color col;
-            if (ColorUtility.TryParseHtmlString(hex, out col))
+            if (ColorUtility.TryParseHtmlString(hex, out Color col))
                 patternColorList.Add(col);
         }
 
@@ -51,68 +61,94 @@ public class FroglopediaGenerator : MonoBehaviour
     {
         foreach (FrogPattern pattern in patterns)
         {
-            List<Color> colorsToUse = pattern.name == "none" ? 
-                new List<Color> { bodyColorList[0] } : bodyColorList;
+            List<Color> colorsToUse =
+                pattern.name == "none"
+                ? new List<Color> { bodyColorList[0] }
+                : bodyColorList;
 
             foreach (Color body in colorsToUse)
             {
                 for (int i = 0; i < frogRenderers.Length; i++)
                 {
-                    Color quadBody = pattern.name == "none" ?
-                        bodyColorList[i % bodyColorList.Count] : // cycle through body colors
-                        body; // use current body color for patterned frogs
-                    
-                    Color patCol = pattern.name == "none" ? quadBody :
-                        i < patternColorList.Count ? patternColorList[i] : Color.white;
-                    
-                    bool discovered = gm.discoveredFrogs.Contains(quadBody + "_" + patCol + "_" + pattern.name);
+                    Color quadBody =
+                        pattern.name == "none"
+                        ? bodyColorList[i % bodyColorList.Count]
+                        : body;
+
+                    Color patCol =
+                        pattern.name == "none"
+                        ? quadBody
+                        : (i < patternColorList.Count
+                            ? patternColorList[i]
+                            : Color.white);
+
+                    bool discovered =
+                        gm.discoveredFrogs.Contains(
+                            quadBody + "_" + patCol + "_" + pattern.name);
 
                     MaterialPropertyBlock block = new MaterialPropertyBlock();
                     frogRenderers[i].GetPropertyBlock(block);
+
                     block.SetTexture("_MainTex", pattern.texture);
                     block.SetColor("_BodyColor", quadBody);
                     block.SetColor("_PatternColor", patCol);
                     block.SetFloat("_Greyscale", discovered ? 0f : 1f);
+
                     frogRenderers[i].SetPropertyBlock(block);
                 }
-                                
+
                 int templateIndex = generatedPages.Count % templateBackgrounds.Length;
 
                 MaterialPropertyBlock bgBlock = new MaterialPropertyBlock();
                 backgroundRenderer.GetPropertyBlock(bgBlock);
                 bgBlock.SetTexture("_BaseMap", templateBackgrounds[templateIndex]);
                 backgroundRenderer.SetPropertyBlock(bgBlock);
+
                 yield return new WaitForEndOfFrame();
 
-                generatedPages.Add(CapturePage());
+                generatedPages.Add(CapturePage(pageCamera));
             }
         }
 
-        generatedPages.Insert(0, TOC);
+        // Front cover
         generatedPages.Insert(0, front);
+
+        // TOC generated from the TOC camera (includes UI)
+        generatedPages.Insert(1, CapturePage(tocCamera));
+
+        // Back cover
         generatedPages.Add(back);
 
         book.bookPages = generatedPages.ToArray();
         book.currentPage = 0;
         book.UpdateSprites();
 
-        Debug.Log("Generated " + generatedPages.Count + " pages");
+        Debug.Log($"Generated {generatedPages.Count} pages");
     }
 
-    Sprite CapturePage()
+    Sprite CapturePage(Camera cam)
     {
         RenderTexture rt = new RenderTexture(512, 768, 16);
-        pageCamera.targetTexture = rt;
-        pageCamera.Render();
 
+        cam.targetTexture = rt;
         RenderTexture.active = rt;
-        Texture2D tex = new Texture2D(512, 768, TextureFormat.RGBA32, false);
-        tex.ReadPixels(new Rect(0, 0, 512, 768), 0, 0);
-        tex.Apply();
-        RenderTexture.active = null;
-        pageCamera.targetTexture = null;
-        rt.Release();
 
-        return Sprite.Create(tex, new Rect(0, 0, 512, 768), new Vector2(0.5f, 0.5f));
+        cam.Render();
+
+        Texture2D tex = new Texture2D(512, 768, TextureFormat.RGBA32, false);
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        tex.Apply();
+
+        cam.targetTexture = null;
+        RenderTexture.active = null;
+
+        rt.Release();
+        Destroy(rt);
+
+        return Sprite.Create(
+            tex,
+            new Rect(0, 0, tex.width, tex.height),
+            new Vector2(0.5f, 0.5f)
+        );
     }
 }
